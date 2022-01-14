56c9c8921d2708d069ac62e27c1ebdb4b3636750
Rushikesh Patil <rushipatil6632@gmail.com>
2022-01-14 09:42:26 +0530

    case statement to check multiple condition

--- empwage.sh
+++ empwage.sh
@@ -4,14 +4,16 @@ isFullTime=1;
 isPartTime=2;
 empRatePerHrs=20;
 randomcheck=$((RANDOM%3));
-if [ $isFullTime -eq $randomcheck ];
-then
-	empHrs=8;
-elif [ $isPartTime -eq $randomcheck ];
-then
-	empHrs=4;
-else
-	empHrs=0;
-fi
-salry=$(($empRatePerHrs*$empHrs))
+case $randomcheck in
+	$isFullTime)
+		empHrs=8
+		;;
+	$isPartTime)
+		empHrs=4
+		;;
+	*)
+		empHrs=0
+esac
+
+salary=$(($empRatePerHrs*$empHrs))
 echo $salary;
